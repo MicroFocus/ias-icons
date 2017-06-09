5a6a6ea9cb4c3748d338dc9771b1b680a611f1d5
HSommer <hope.sommer@microfocus.com>
2017-06-09 09:38:12 -0600

    bunch of stuff

--- docs/app.js
+++ docs/app.js
@@ -1,11 +1,38 @@
-function AppController($scope, ICONS) {
-    $scope.icons = ICONS;
-}
+// import { routes } from './config/routes';
+//
+    app = angular.module('app', ['ng-ias','ui.router']);
 
-angular.module('app', ['ng-ias'])
-    .controller('AppController', AppController);
 
-angular.element(document).ready(function() {
-    angular.bootstrap(document, ['app']);
-});
+    app.config(function($stateProvider, $urlServiceProvider) {
+        $urlServiceProvider.rules.otherwise({state: 'app.icons.table'});
+
+        $stateProvider.state('app', {
+            url: '/app',
+            component: 'app',
+            abstract: true,
+        });
+
+        $stateProvider.state('app.icons', {
+            url: '/icons?query',
+            component: 'icons',
+            abstract: true,
+        });
+
+        $stateProvider.state('app.icons.table', {
+            url: '/table',
+            templateUrl: '/docs/components/icons/icons-table.component.html'
+        });
+
+        $stateProvider.state('app.icons.tile', {
+            url: '/tile',
+            templateUrl: '/docs/components/icons/icons-tile.component.html'
+        });
+
+    });
+
+
+    angular.element(document).ready(function() {
+        angular.bootstrap(document, ['app']);
+    });
+
 
