e56bf3d592de65509345a446b0e6a54e67ae38a6
Jackson Lauder <jacksonelauder@gmail.com>
2021-08-31 14:31:59 -0600

    Added menu component to tiles in tile view

--- src/app/tiled-list/tiled-list.component.ts
+++ src/app/tiled-list/tiled-list.component.ts
@@ -1,19 +1,39 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { IconInfo } from '../../common/types/icon-info.type';
 import { Observable } from 'rxjs';
+import { MenuComponent, MenuTriggerDirective } from '@ux-aspects/ux-aspects';
 
 @Component({
     selector: 'app-tiled-list',
     templateUrl: './tiled-list.component.html',
-    styleUrls: ['./tiled-list.component.scss']
+    styleUrls: ['./tiled-list.component.scss'],
 })
 export class TiledListComponent implements OnInit {
     @Input() iconInfoList$: Observable<IconInfo[]>;
 
-    constructor() {
+    @ViewChild(MenuTriggerDirective) menu: MenuTriggerDirective;
+
+    iconName: null;
+
+    constructor() {}
+
+    ngOnInit(): void {}
+
+    click(name) {
+        this.iconName = name;
     }
 
-    ngOnInit(): void {
+    copyIconName(): void {
+        console.log('Copied: ' + this.iconName);
+        document.addEventListener('copy', (e: ClipboardEvent) => {
+            e.clipboardData.setData('text/plain', this.iconName);
+            e.preventDefault();
+            document.removeEventListener('copy', null);
+        });
+        document.execCommand('copy');
     }
 
+    downloadIconSVG(): void {
+        console.log('Clicked Download Icon SVG');
+    }
 }
