b5c7965e52e15db15cb29f7b19ec4080b2add0c5
Jo Windley <joseph.windley@microfocus.com>
2022-09-27 09:36:46 -0600

    changes to app.component.ts for search not being case sensitive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,8 +16,10 @@ import {
     tap,
     withLatestFrom,
 } from 'rxjs/operators';
+import { IconType } from '@ux-aspects/ux-aspects';
+import { LowerCasePipe } from '@angular/common';
 
-const FULL_ICON_LIST = require('../../dist/generated-font/dist/ias-icons.json');
+const FULL_ICON_LIST = require('../../package/dist/ias-icons.json');
 
 @Component({
     selector: 'app-root',
@@ -56,12 +58,20 @@ export class AppComponent implements OnInit, OnDestroy {
                 map((icons: IconInfo[]) => {
                     return icons.sort((a: IconInfo, b: IconInfo) =>
                         a.glyph.localeCompare(b.glyph)
-                    );
+                    ) .map((icon) => {
+                        let iconCopy: IconInfo = icon;
+                        iconCopy.name = iconCopy.name.toLocaleLowerCase();
+                        return iconCopy;
+                        // Might have to do a deep copy here instead of normal copy (still might be referencing the original icon and changing at the source not the copy)
+                    });
                 })
             ),
             this.searchFormControl.valueChanges.pipe(
                 debounceTime(200),
-                startWith('')
+                startWith(''),
+                map((search: string) => {
+                    return search.toLowerCase();
+                })
             ),
         ])
             .pipe(
@@ -70,9 +80,10 @@ export class AppComponent implements OnInit, OnDestroy {
                         let combined_filtered_array = []
                         let filter_by_name= icons.filter((icon: IconInfo) => {
                             return icon.name.indexOf(search) > -1;
+                            // TODO: make icon name and search value lowercase then check if name appears in search
                         });
                         let filter_by_uses= icons.filter((icon: IconInfo) => {
-                            
+                            // TODO: make icon name and search value lowercase then check if name appears in search | lowercase
                             return icon.uses.indexOf(search) > -1;
                         });
                         combined_filtered_array.push(...filter_by_name)
