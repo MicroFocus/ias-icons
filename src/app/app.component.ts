be492c524d80a3465cac85691b017ebee8acb2e4
Lynn Christensen <lynn.christensen@microfocus.com>
2022-12-06 11:20:29 -0700

    fixing broken IAS icons site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,12 +16,6 @@ import {
     tap,
     withLatestFrom,
 } from 'rxjs/operators';
-import { IconType } from '@ux-aspects/ux-aspects';
-import { LowerCasePipe } from '@angular/common';
-import { SearchFiltersService } from 'src/app/app.filters'
-import { appendFile } from 'fs';
-
-// const FULL_ICON_LIST = require('../../package/dist/ias-icons.json');
 
 const FULL_ICON_LIST = require('../../dist/generated-font/dist/ias-icons.json');
 
@@ -40,13 +34,6 @@ export class AppComponent implements OnInit, OnDestroy {
 
     showDetailedList: boolean;
 
-    showDropdownFilter: boolean;
-
-    constructor(
-        public searchFilterService: SearchFiltersService
-    ) {}
-
-
     ngOnInit(): void {
         this.iconInfoListSubscription = this.subscribeToIconInfoListChanges();
     }
@@ -63,34 +50,29 @@ export class AppComponent implements OnInit, OnDestroy {
         this.showDetailedList = true;
     }
 
-    onFilterDropdownClick(): void {
-        this.showDropdownFilter = true;
-    }
-
     private subscribeToIconInfoListChanges(): Subscription {
         return combineLatest([
             of(FULL_ICON_LIST).pipe(
                 map((icons: IconInfo[]) => {
                     return icons.sort((a: IconInfo, b: IconInfo) =>
                         a.glyph.localeCompare(b.glyph)
-                    ) 
+                    );
                 })
             ),
             this.searchFormControl.valueChanges.pipe(
                 debounceTime(200),
-                startWith(''),
+                startWith('')
             ),
         ])
             .pipe(
-                map(([icons, search]) => { // Do filter comparison in here
+                map(([icons, search]) => {
                     if (search) {
                         let combined_filtered_array = []
                         let filter_by_name= icons.filter((icon: IconInfo) => {
-                            // return icon.name.indexOf(search) > -1; 
-                            return icon.name.toLocaleLowerCase().includes(search.toLowerCase()); // equals was returning strings as not equal, so using .includes compares them properly
-
+                            return icon.name.indexOf(search) > -1; 
                         });
                         let filter_by_uses= icons.filter((icon: IconInfo) => {
+
                             return icon.uses.indexOf(search) > -1;
                         });
                         combined_filtered_array.push(...filter_by_name)
@@ -98,8 +80,6 @@ export class AppComponent implements OnInit, OnDestroy {
                         combined_filtered_array = [...new Set(combined_filtered_array)]
                         
                         return combined_filtered_array 
-                        // Returning the icon if it can find the search within the arrary
-
                     }
 
                     return icons;
