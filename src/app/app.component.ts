75320a1aae47283b3e0a04e4ca1832d87bbeabe0
Lynn Christensen <lynn.christensen@microfocus.com>
2022-12-06 09:08:16 -0700

    Search improved to find all string values no matter capitalization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,9 @@ import {
     withLatestFrom,
 } from 'rxjs/operators';
 
+import { SearchFiltersService } from 'src/app/app.filters'
+import { appendFile } from 'fs';
+
 const FULL_ICON_LIST = require('../../dist/generated-font/dist/ias-icons.json');
 
 @Component({
@@ -34,6 +37,12 @@ export class AppComponent implements OnInit, OnDestroy {
 
     showDetailedList: boolean;
 
+    showDropdownFilter: boolean;
+
+    constructor(
+        public searchFilterService: SearchFiltersService
+    ) {}
+
     ngOnInit(): void {
         this.iconInfoListSubscription = this.subscribeToIconInfoListChanges();
     }
@@ -48,6 +57,10 @@ export class AppComponent implements OnInit, OnDestroy {
 
     onListViewClicked(): void {
         this.showDetailedList = true;
+    }
+
+    onFilterDropdownClick(): void {
+        this.showDropdownFilter = true;
     }
 
     private subscribeToIconInfoListChanges(): Subscription {
@@ -65,11 +78,12 @@ export class AppComponent implements OnInit, OnDestroy {
             ),
         ])
             .pipe(
-                map(([icons, search]) => {
+                map(([icons, search]) => { //Do filter comparison in here
                     if (search) {
                         let combined_filtered_array = []
                         let filter_by_name= icons.filter((icon: IconInfo) => {
-                            return icon.name.indexOf(search) > -1;
+                            // return icon.name.indexOf(search) > -1;
+                            return icon.name.toLocaleLowerCase().includes(search.toLowerCase()); // equals was returning strings as not equal, so using .includes compares them properly
                         });
                         let filter_by_uses= icons.filter((icon: IconInfo) => {
                             
@@ -80,6 +94,8 @@ export class AppComponent implements OnInit, OnDestroy {
                         combined_filtered_array = [...new Set(combined_filtered_array)]
                         
                         return combined_filtered_array 
+                        // Returning the icon if it can find the search within the array
+
                     }
 
                     return icons;
